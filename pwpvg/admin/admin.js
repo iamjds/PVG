cacabd0043993306928f6c39e1c5b544ae774de1
Jake Schaap <jakeschaap@gmail.com>
2021-07-22 16:02:03 -0400

    Update admin.js

--- pwpvg/admin/admin.js
+++ pwpvg/admin/admin.js
@@ -4,4 +4,5 @@ jQuery(function($){
     $('#pv_features_attributes textarea').prop('disabled', true);
     $('#pv_plant_needs_attributes input').prop('disabled', true);
     $('#pv_plant_needs_attributes textarea').prop('disabled', true);
+    $('#pv_growing_info_attributes input').prop('disabled', true);
 });
